3a862aa2ffc909862bfcda8cd4cd89354a69831d
Andrew Carr <andrew@2metres.com>
2017-12-12 23:32:34 +1100

    Create responses using graphql mutations

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
-const Form = () => (
-  <form>
-    This is the form
-  </form>
-)
+const Form = ({ mutate }) => {
+  const handleClick = () => {
+    mutate({
+      variables: {
+        name: 'Andrew Carr',
+        attending: true,
+        dietaryRestrictions: false,
+        dietaryRestrictionDescription: '',
+        song: 'Keep em seperated',
+      },
+    }).then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
 
-export default Form
+  return (
+    <button onClick={handleClick}> Click Me! </button>
+  )
+}
+
+const createResponse = gql`
+  mutation createResponse(
+    $name: String!,
+    $attending: Boolean!
+    $dietaryRestrictions: Boolean!,
+    $dietaryRestrictionDescription: String!,
+    $song: String!
+  ) {
+    createResponse(
+      name: $name,
+      attending: $attending,
+      dietaryRestrictions: $dietaryRestrictions,
+      dietaryRestrictionDescription: $dietaryRestrictionDescription,
+      song: $song
+    ) {
+      id
+    }
+  }
+`
+
+const FormWithData = graphql(createResponse)(Form)
+
+export default FormWithData
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,26 @@
 import React from 'react'
-import { Router } from 'react-static'
 import Routes from 'react-static-routes'
+import { Router } from 'react-static'
+import { ApolloClient, ApolloProvider } from 'react-apollo'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 import { Nav } from '../components'
 
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'https://api.graphcms.com/simple/v1/cjaneflbw334r01578tve6v3r' }),
+  cache: new InMemoryCache(),
+})
+
 const App = () => (
-  <Router>
-    <div id="content">
-      <Nav />
-      <Routes />
-    </div>
-  </Router>
+  <ApolloProvider client={client}>
+    <Router>
+      <div id="content">
+        <Nav />
+        <Routes />
+      </div>
+    </Router>
+  </ApolloProvider>
 )
 
 export default App
