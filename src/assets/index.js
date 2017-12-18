584f233da54727a3b4ba00ced0ccb43cc9b74c88
Andrew Carr <andrew@2metres.com>
2017-12-18 14:12:07 +1100

    Additional icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,23 +1,31 @@
+import bed from './bed.svg'
 import bus from './bus.svg'
 import camera from './camera.svg'
+import car from './car.svg'
 import couple from './couple.svg'
 import cloche from './cloche.svg'
 import clock from './clock.svg'
 import dancing from './dancing.svg'
+import doorSign from './doorSign.svg'
 import drinks from './drinks.svg'
 import light from './light.svg'
+import music from './music.svg'
 import plate from './plate.svg'
 import record from './record.svg'
 
 export {
+  bed,
   bus,
   camera,
+  car,
   couple,
   cloche,
   clock,
   dancing,
+  doorSign,
   drinks,
   light,
+  music,
   plate,
   record,
 }
