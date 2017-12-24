811e23614ad6401531f33c76e3381352d8936dd9
Andrew Carr <andrew@2metres.com>
2017-12-24 19:56:41 +1300

    Add iterator for FAQs

--- src/components/FAQs/index.js
+++ src/components/FAQs/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
+import { paramCase } from 'change-case'
 import Markdown from 'react-markdown'
 
 import styles from './module.scss'
@@ -9,7 +10,7 @@ const FAQs = ({ items }) => (
     <h2 className={styles.title}>Things to know</h2>
     <div className="row">
       { items.map(item => (
-        <div className={classNames(styles.item, 'col-md-6')}>
+        <div key={paramCase(item.title)} className={classNames(styles.item, 'col-md-6')}>
           <h3 className={styles.itemTitle}>{item.title}</h3>
           <Markdown className={styles.itemBody}>{item.body}</Markdown>
         </div>))
